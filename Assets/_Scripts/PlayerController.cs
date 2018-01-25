5de347b7897f5dc46c8181d7d574aa8052a428ca
Teefuhnee <teefuhnee>
2018-01-24 18:05:41 -0800

    Create moving player who shoots water bullets

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float m_carMove;
+	public float m_playerSpeed = 11.0f;
+	public float m_playerMove;
+	public GameObject m_WaterPrefab;
+	public Boundary m_boundary;
+
+	private Rigidbody m_rigidbody;
+
+	private void Awake ()
+	{
+		m_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		MovePlayer ();
+	}
+
+	private void Update()
+	{
+		Shoot();
+	}
+
+	private void Shoot()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			Instantiate (m_WaterPrefab, transform.position, transform.rotation);
+		}
+	}
+	private void MovePlayer()
+	{
+		m_playerMove = Input.GetAxis("Horizontal") * Time.deltaTime * m_playerSpeed;
+		transform.Translate(m_playerMove, 0, 0);
+
+		m_rigidbody.position = new Vector3 
+			(
+				Mathf.Clamp (m_rigidbody.position.x, m_boundary.xMin, m_boundary.xMax), 
+				0.0f, 
+				0.0f
+			);
+	}
+}
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax;
+}
+
---
+++ Assets/_Scripts/WaterBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBullet : MonoBehaviour {
+
+	public float m_bulletSpeed = 10f;
+
+	private Rigidbody m_Rigidbody;
+
+	private void Awake()
+	{
+		m_Rigidbody = GetComponent<Rigidbody>();
+
+		Invoke("DestroySelf", 2.0f);
+	}
+	
+	private void FixedUpdate()
+	{
+		m_Rigidbody.MovePosition(m_Rigidbody.position + transform.forward
+			 					 * m_bulletSpeed * Time.deltaTime);
+	}
+
+	private void DestroySelf()
+	{
+		Destroy(gameObject);
+	}
+}
+
